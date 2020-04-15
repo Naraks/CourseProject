266eb56bae05cca6447bc8b0b7e7c2ee4cdc8353
Sergey <Progenitor123@gmail.com>
2020-04-15 11:36:48 +0700

    init

---
+++ myappserver/src/main/java/com/github/naraks/myappserver/MyappserverApplication.java
@@ -0,0 +1,13 @@
+package com.github.naraks.myappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappserverApplication.class, args);
+	}
+
+}
---
+++ myappserver/src/test/java/com/github/naraks/myappserver/MyappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.naraks.myappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
